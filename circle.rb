c42d7e94d562913275e14bd0c302562b65820f90
Alberto Ramponi <alberto.ramponi@gmail.com>
2020-01-27 17:08:14 +0100

    .

---
+++ circle.rb
@@ -0,0 +1,12 @@
+
+radius = 5.0
+perimeter = 0.0
+area = 0.0
+
+puts "Insert the radius"
+
+perimeter = 2*3.14*radius
+area = 3.14*radius*radius
+
+puts "Perimeter: #{perimeter}"
+puts "Area: #{area}"
